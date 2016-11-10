5b63ceb9539794731568363332f51e31d608dcd5
John <johnjwesthoff@gmail.com>
2016-11-09 23:26:20 -0500

    ok

--- t.py
+++ t.py
@@ -1,27 +1,50 @@
-import sys, pygame, math, hexgrid.grid
+import sys, pygame, math, grid
 
 
 pygame.init()
 
-size = width, height = 336, math.ceil(336*3/4)
+size = width, height = 656, int(656*3/4)
 black = 0, 0, 0
+Vx, Vy = 0, 0
+dvx, dvy = 0, 0
 
 screen = pygame.display.set_mode(size)
-grid   = hexgrid.grid.Grid()
+g   = grid.Grid(600,600,64)
 X, Y = (5, 5)
 
-grid.setTile(X, Y, hexgrid.grid.WATER)
-for (i, j) in grid.getBetween(X, Y, 2, 5):
-    grid.setTile(i, j, hexgrid.grid.ROCKS)
+g.setTile(X, Y, grid.WATER)
+for (i, j) in g.getBetween(X, Y, 2, 5):
+    g.setTile(i, j, grid.ROCKS)
 
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT: sys.exit()
         if event.type == pygame.MOUSEBUTTONDOWN:
-                mx, my = event.pos
-                xx, yy = grid.rectToHex(mx, my)
-                grid.setTile(xx, yy, hexgrid.grid.WATER)
-
+            mx, my = event.pos
+            xx, yy = g.rectToHex(mx + Vx, my + Vy)
+            g.setTile(xx, yy, grid.WATER)
+        if event.type == pygame.KEYUP:
+            if event.key == 276:
+                dvx = 0
+            elif event.key == 274:
+                dvy = 0
+            elif event.key == 275:
+                dvx = 0
+            elif event.key == 273:
+                dvy = 0
+        if event.type == pygame.KEYDOWN:
+            if event.key == 276:
+                dvx = -1
+            elif event.key == 274:
+                dvy = 1
+            elif event.key == 275:
+                dvx = 1
+            elif event.key == 273:
+                dvy = -1
+                
+    Vx += dvx
+    Vy += dvy
+    print Vx, Vy
     screen.fill(black)
-    grid.draw(screen)
+    g.draw(screen, Vx, Vy)
     pygame.display.flip()
