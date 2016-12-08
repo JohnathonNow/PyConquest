79dedf8e906f2a2e841d1e79ac4a16b2e8a4c555
John <johnjwesthoff@gmail.com>
2016-12-07 21:37:25 -0500

    Fixed inputbox for windows

--- inputbox.py
+++ inputbox.py
@@ -25,7 +25,7 @@ def get_key():
 
 def display_box(screen, message):
   "Print a message in a box in the middle of the screen"
-  fontobject = pygame.font.Font(None,18)
+  fontobject = pygame.font.SysFont("monospace", 15)
   pygame.draw.rect(screen, (0,0,0),
                    ((screen.get_width() / 2) - 100,
                     (screen.get_height() / 2) - 10,
